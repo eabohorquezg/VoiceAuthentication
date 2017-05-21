78baba1af31dba3c128548604a3cb41d9b8bee2e
Edwin Alexander <eabohorquezg@unal.edu.co>
2017-05-20 20:15:30 -0500

    The speech recognition algorithm is terminated

--- src/Model/Test.java
+++ src/Model/Test.java
@@ -3,10 +3,13 @@ package Model;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,36 +17,38 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  */
 
 public class Test{
-    
-    public static void voiceRecorder(String nameAudio) throws UnsupportedAudioFileException, IOException{
+       
+    public static double[] voiceRecorder(String nameAudio) throws UnsupportedAudioFileException, IOException{
         int seconds = 3;
         SoundRecorder sr = new SoundRecorder(nameAudio,seconds);
         sr.voiceRecorder();        
         File soundFile = new File(nameAudio+".wav");
         AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);                                    
         AudioPreProcessor ap = new AudioPreProcessor(ais);   
-        MFCC mfcc = new MFCC(11025.0f);                
-        Vector<double[]> coefficients = mfcc.process(ap);            
-        System.out.println("size = "+coefficients.size());
-        System.out.println("coefficients : ");
+        MFCC mfcc = new MFCC(11025.0f);   
+        List<Double> list =  new ArrayList<Double>();               
+        Vector<double[]> coefficients = mfcc.process(ap);                    
         for (int t = 0; t < coefficients.size(); t++) {
-            for (int i = 0; i < coefficients.get(t).length; i++) {        
-                System.out.print(coefficients.get(t)[i]+" ");                
-            }            
-            System.out.println("");
-        }                                               
+            for (int i = 0; i < coefficients.get(t).length; i++) {                        
+                    list.add(1.0*coefficients.get(t)[i]);
+            }                        
+        }                
+        return list.stream().mapToDouble(Double::doubleValue).toArray();        
     }
     
     public static void main(String args[]) throws UnsupportedAudioFileException, IOException, InterruptedException{
-        
-        Test.voiceRecorder("patternVoice");
-        System.out.println("-----------------------------------------");
-        Thread.sleep(3000);
-        Test.voiceRecorder("testVoice");
-        
-//        TODO: Implement DTW for to compare coefficients 
-        
-        
-    }
+        JOptionPane.showMessageDialog (null, "Registre su voz", "Mensaje del sistema", JOptionPane.INFORMATION_MESSAGE);
+        double[] coefficientsPatternVoice = Test.voiceRecorder("patternVoice");        
+        JOptionPane.showMessageDialog (null, "Autentiquese", "Mensaje del sistema", JOptionPane.INFORMATION_MESSAGE);
+        double[] coefficientsTestVoice = Test.voiceRecorder("testVoice");
+        DTW dtw = new DTW(coefficientsPatternVoice, coefficientsTestVoice);                                                                                              
+        System.out.println("distance = "+dtw.warpingDistance);
+        if( dtw.warpingDistance <= 0.05 ){
+            JOptionPane.showMessageDialog (null, "ACCESO CONCEDIDO", "Mensaje de sistema", JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog (null, "ACCESO DENEGADO", "Mensaje de sistema", JOptionPane.ERROR_MESSAGE);
+        }                                                                                                                
+        System.exit(0);
+    }                            
     
 }
